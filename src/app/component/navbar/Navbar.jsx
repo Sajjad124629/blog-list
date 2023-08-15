9f96b81a5514a7c485caa6b01e84274f80727edb
Sajjad124629 <sh8170468@gmail.com>
2023-08-15 19:05:36 +0600

    Date:15-8-2023

--- src/app/component/navbar/Navbar.jsx
+++ src/app/component/navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import styles from './navbar.css'
+import styles from './navbar.module.css'
 import Link from 'next/link';
 const Navbar = () => {
     return (
         <div className='container justify-center mt-10'>
             <nav>
-                <ul className={`flex justify-center navStyle`}>
+                <ul className={`flex justify-center ${styles.navStyle}`}>
                     <li><Link href={`/`}>Home</Link></li>
                     <li><Link href={`/blog`}>Blog</Link></li>
                 </ul>
